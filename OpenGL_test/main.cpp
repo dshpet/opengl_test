41e122f0af216905533a49c7baf4e1e43e18badd
dshpet <dshpet@gmail.com>
2017-02-17 18:19:46 +0200

    fix removed item

--- OpenGL_test/main.cpp
+++ OpenGL_test/main.cpp
@@ -217,7 +217,7 @@ int main()
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(lightVertices), lightVertices, GL_STATIC_DRAW);
 	
 	// Vertex pos
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)nullptr);
